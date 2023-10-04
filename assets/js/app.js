2469032aa274f1b382667032404e0cc1c6694338
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2023-10-03 22:41:01 -0600

    Hice una promesa a una API para que me de los valore de un usuario

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,46 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
+// console.log($n);
+// console.log($b);
+// console.log($l);
+
+
+//el código conecta con un api para recibir un archivo JSON y mostrarlo en cosola
+
+
+async function displayUser(username) {
+  $n.textContent = 'cargando...'; 
+
+  try {const response = await fetch(`${usersEndpoint}/${username}`);
+  let data = await response.json();
+ 
+    
+  } catch (error) {
+    console.log("error intentando recibir el dato ")
+  }
+
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  let data = await response.json();
+  
+
+
+
+  //console.log(data);
+  //console.log(data.name);
+  
+ $n.textContent = data.name;
+  $b.textContent = data.blog;
+  $l.textContent = data.location;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`
 }
 
 displayUser('stolinski').catch(handleError);
